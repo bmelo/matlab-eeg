2867279ed7e79cd0f49c1e56aaf54e06c4c31fe1
Bruno Melo <bruno.melo@idor.org>
2017-05-17 13:54:08 -0300

    Final version with second level analysis.

---
+++ src/steps/export_SL_excel.m
@@ -0,0 +1,72 @@
+function export_SL_excel( config, statsfile )
+%EXPORT_SL_EXCEL Summary of this function goes here
+%   Detailed explanation goes here
+
+load(statsfile, 'results', 'channels');
+
+files = {'pEEG_global' 'pEEG_8_13' 'pEEG_13_26' 'pEEG_26_45'};
+
+% Exporting excel
+utils.run('vendors/xlwrite/libsetup.m');
+fmt = get(0,'Format');
+format longG;
+header = [{''} channels];
+threshold = ['limiar' {.05}];
+
+%% REPORT DIFF TESTS
+% 1 - F-TEST (VARIANCE NORMAL DISTRIBUTION)
+% 2 - T-TEST
+% 3 - WILCOXON
+report_diff_test( 'teste-T', 'ttest' );
+report_diff_test( 'teste-F', 'ftest' );
+report_diff_test( 'wilcoxon', 'wilcoxon' );
+
+%% REPORT FRIEDMAN TEST
+for k = 1:length(files)
+    file = files{k};
+    xlsdata.(file) = {
+        header
+        ['POWER'   num2cell( [results(k).stats.sl.friedman.power(:).p] )]
+        ['ERD/ERS' num2cell( [results(k).stats.sl.friedman.sync(:).p] ) ]
+        ''
+        ''
+        threshold
+    };
+end
+
+xlsname = fullfile(config.outdir_base, 'friedman.xlsx');
+utils.geraOut( xlsname, xlsdata );
+
+format(fmt); % Back to default format
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% REPORT TEST-F (VARIANCE NORMAL DISTRIBUTION)
+function report_diff_test( outfile, testname )
+
+for nF = 1:length(files)
+    file = files{nF};
+    result = results(nF).stats.sl.(testname);
+    xlsdata.(file) = {
+        'POWER'
+        header
+        ['T vs N' num2cell(result.power.T_N_p)]
+        ['A vs N' num2cell(result.power.A_N_p)]
+        ['T vs A' num2cell(result.power.T_A_p)]
+        ''
+        'ERD/ERS'
+        header
+        ['T vs N' num2cell(result.sync.T_N_p) ]
+        ['A vs N' num2cell(result.sync.A_N_p) ]
+        ['T vs A' num2cell(result.sync.T_A_p) ]
+        ''
+        ''
+        threshold
+    };
+end
+xlsname = fullfile(config.outdir_base, [outfile '.xls']);
+utils.geraOut( xlsname, xlsdata );
+
+end
+
+end
+
--- src/steps/second_level.m
+++ src/steps/second_level.m
@@ -30,70 +30,9 @@ for k = 1:length(files)
     results(k).stats.sl.wilcoxon.sync = testChannels( msEEG, 'wilcoxon' );
 end
 
-save('results-SL.mat', 'results');
-%load('results-SL.mat');
-%load('channels');
+outfile = fullfile( config.outdir_base, 'results-SL.mat' );
+save(outfile, 'results', 'channels');
 
-% Exporting excel
-utils.run('vendors/xlwrite/libsetup.m');
-fmt = get(0,'Format');
-format longG;
-header = [{''} channels];
-threshold = ['limiar' {.05}];
-
-%% REPORT DIFF TESTS
-% 1 - F-TEST (VARIANCE NORMAL DISTRIBUTION)
-% 2 - T-TEST
-% 3 - WILCOXON
-report_diff_test( 'teste-T', 'ttest' );
-report_diff_test( 'teste-F', 'ftest' );
-report_diff_test( 'wilcoxon', 'wilcoxon' );
-
-%% REPORT FRIEDMAN TEST
-for k = 1:length(files)
-    file = files{k};
-    xlsdata.(file) = {
-        header
-        ['POWER'   num2cell( [results(k).stats.sl.friedman.power(:).p] )]
-        ['ERD/ERS' num2cell( [results(k).stats.sl.friedman.sync(:).p] ) ]
-        ''
-        ''
-        threshold
-    };
-end
-
-xlsname = fullfile(config.outdir_base, 'friedman.xlsx');
-utils.geraOut( xlsname, xlsdata );
-
-format(fmt); % Back to default format
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% REPORT TEST-F (VARIANCE NORMAL DISTRIBUTION)
-function report_diff_test( outfile, testname )
-
-for nF = 1:length(files)
-    file = files{nF};
-    result = results(nF).stats.sl.(testname);
-    xlsdata.(file) = {
-        'POWER'
-        header
-        ['T vs N' num2cell(result.power.T_N_p)]
-        ['A vs N' num2cell(result.power.A_N_p)]
-        ['T vs A' num2cell(result.power.T_A_p)]
-        ''
-        'ERD/ERS'
-        header
-        ['T vs N' num2cell(result.sync.T_N_p) ]
-        ['A vs N' num2cell(result.sync.A_N_p) ]
-        ['T vs A' num2cell(result.sync.T_A_p) ]
-        ''
-        ''
-        threshold
-    };
-end
-xlsname = fullfile(config.outdir_base, [outfile '.xls']);
-utils.geraOut( xlsname, xlsdata );
-
-end
+export_SL_excel( outfile );
 
 end
