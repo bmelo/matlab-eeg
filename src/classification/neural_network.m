57189080b3f2df7952452940bf3f8ea18a805cd8
Bruno Melo <bruno.melo@idor.org>
2017-05-25 12:45:11 -0300

    Exporting neural network results. Needing implementation of leave one subject out - group analysis.

--- src/classification/neural_network.m
+++ src/classification/neural_network.m
@@ -1,34 +1,68 @@
 function neural_network(config)
 
 files = {'pEEG_global' 'pEEG_8_13' 'pEEG_13_26' 'pEEG_26_45'};
+subjs = config.subjs;
 
-for k = 1:length(files)
-    file  = files{k};
+for nS = subjs
+    config.subjs = nS;
     
-    group = group_matrix_eeg(config, file);
-    srate = group.srate;
-    channels = group.channels;
+    for k = 1:length(files)
+        file  = files{k};
+        
+        group = group_matrix_eeg(config, file);
+        srate = group.srate;
+        channels = group.channels;
+        
+        pEEG.TASK_T = [group.data(:).TASK_T];
+        pEEG.TASK_A = [group.data(:).TASK_A];
+        clear group;
+        
+        syncEEG = epochs_apply_matrices(@erd_ers, pEEG, srate, srate/5, [srate*5 srate*10] );
+        perc = size(syncEEG.TASK_T,3) / size(pEEG.TASK_T,3);
+        pEEG = epochs_apply_matrices( @window_func, pEEG, srate, srate/5, @mean );
+        
+        srate = srate * perc;
+        
+        % Preparing matrix to use in classifier
+        if ~exist('mFeats', 'var')
+            mFeats = prepare_matrix( pEEG, srate );
+        else
+            mFeats = [mFeats prepare_matrix( pEEG, srate )];
+        end
+        mFeats = [mFeats prepare_matrix( syncEEG, srate )];
+    end
     
-    pEEG.TASK_T = [group.data(:).TASK_T];
-    pEEG.TASK_A = [group.data(:).TASK_A];
-    clear group;
-    
-    syncEEG = epochs_apply_matrices(@erd_ers, pEEG, srate, srate/5, [srate*5 srate*10] );
-    perc = size(syncEEG.TASK_T,3) / size(pEEG.TASK_T,3);
-    pEEG = epochs_apply_matrices( @window_func, pEEG, srate, srate/5, @mean );
-    
-    srate = srate * perc;
-    
-    % Preparing matrix to use in classifier
-    mFeats = prepare_matrix( pEEG, srate );
-    mFeats = [mFeats prepare_matrix( syncEEG, srate )];
-    features = prepare_features(mFeats);
+    % For global Analysis
+    if ~exist('mFeatsGlobal', 'var')
+        mFeatsGlobal = mFeats;
+    else
+        mFeatsGlobal = [mFeatsGlobal mFeats];
+    end
     
     % Testing neural network
+    feats = prepare_features(mFeats);
+    
     net = patternnet(10);
-    net = train(net, features.data, features.classes);
-    view(net);
+    net = train(net, feats.features, feats.classes);
+    %view(net);
+    y = net(feats.features);
+    plotconfusion( feats.classes, y );
+    utils.imgs.print_fig( fullfile( config.imgsexport_dir, sprintf('SUBJ%03d.png', nS) ) );
+    
+    clear mFeats net y feats pEEG syncEEG;
 end
+
+
+%% GLOBAL
+% Testing neural network
+feats = prepare_features(mFeatsGlobal);
+
+net = patternnet(10);
+net = train(net, feats.features, feats.classes);
+%view(net);
+y = net(feats.features);
+plotconfusion( feats.classes, y );
+utils.imgs.print_fig( fullfile( config.imgsexport_dir, sprintf('GLOBAL.png', nS) ) );
 
 %y = net( features.data );
 %perf = perform(net, features.classes, y);
