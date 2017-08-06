36eef4b2432dae2b414c185fd3631173b4746d96
Bruno Melo <bruno.raphael@gmail.com>
2017-08-06 09:23:56 -0300

    Input folder change.

--- src/steps/extract_features.m
+++ src/steps/extract_features.m
@@ -9,6 +9,9 @@ for subjN = config.subjs
     subj = sprintf('%s%03d', config.subj_prefix, subjN);
     subjdir_in = fullfile( config.preproc_dir, subj );
     subjdir_out = fullfile( config.outdir_base, 'FEATS', subj );
+    if ~isdir( subjdir_out )
+        mkdir( subjdir_out )
+    end
     
     fprintf('\n####   FEATURE EXTRACTION - %s   ####\n\n', subj);
     
@@ -26,25 +29,27 @@ for subjN = config.subjs
     end
     
     % Separating each band
-    EEG = eeg_load( subjdir_in, 'bcEEG' );
-    srate = EEG(1).srate;
-    for nB = 1:length(config.bands)
-        band = config.bands(nB, :);
-        
-        %% Electrical activity (EEG)
-        overlap  = srate/2;
-        EEGfeats = prepare_features( EEG(nB), srate, overlap );
-        eeg_save( subjdir_out, gen_filename('eeg_feats', band), EEGfeats );
-        
-        %% POWER
-        pEEG      = epochs_apply(@power_eeg, EEG(nB));
-        pEEGfeats = prepare_features( pEEG, srate, overlap );
-        eeg_save( subjdir_out, gen_filename('power_feats', band), pEEGfeats );
-        
-        %% ERD/ERS
-        erdEEG      = epochs_apply(@erd_ers, EEG(nB), [srate*5 srate*10] );
-        erdEEGfeats = prepare_features( erdEEG, srate, overlap );
-        eeg_save( subjdir_out, gen_filename('erders_feats', band), erdEEGfeats );
+    if config.proc.features.erders
+        EEG = eeg_load( subjdir_in, 'bcEEG' );
+        srate = EEG(1).srate;
+        for nB = 1:length(config.bands)
+            band = config.bands(nB, :);
+
+            %% Electrical activity (EEG)
+            overlap  = srate/2;
+            EEGfeats = prepare_features( EEG(nB), srate, overlap );
+            eeg_save( subjdir_out, gen_filename('eeg_feats', band), EEGfeats );
+
+            %% POWER
+            pEEG      = epochs_apply(@power_eeg, EEG(nB));
+            pEEGfeats = prepare_features( pEEG, srate, overlap );
+            eeg_save( subjdir_out, gen_filename('power_feats', band), pEEGfeats );
+
+            %% ERD/ERS
+            erdEEG      = epochs_apply(@erd_ers, EEG(nB), [srate*5 srate*10] );
+            erdEEGfeats = prepare_features( erdEEG, srate, overlap );
+            eeg_save( subjdir_out, gen_filename('erders_feats', band), erdEEGfeats );
+        end
     end
 end
 
