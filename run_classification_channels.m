add3a48969ad9c62ea4e1dd61392abf2b5425f39
Bruno Melo <bruno.raphael@gmail.com>
2017-09-04 09:23:56 -0300

    Fix classification channels.

--- run_classification_channels.m
+++ run_classification_channels.m
@@ -10,21 +10,24 @@ clc;
 %% Setup of processing
 config = setup('neutral_length', 10);
 
-config.features = {'power_rel_feats' 'erders_feats'};
+config.features = {'l_power_rel_feats' 'l_erders_feats'};
 %config.features = {'l_eeg_feats'};
 config.measures = {'median'};
-config.outdir = 'STATS/CLASSIFICATION/ANN/CHANNELS/EEG-prev';
+config.outdir = 'STATS/CLASSIFICATION/ANN/CHANNELS/EEG';
 
 config.bands = [8 13; 13 26];
 
 config.cross.type = 'kfold';
 config.cross.k = 8;
-config.cross.repetitions = 5;
+config.cross.repetitions = 10;
 
 auxchs = load('extra/channels');
 auxchs = auxchs.channels;
 config.subjs = 1:14;
 config.subjs = [1 2 8 9];
+
+% Don't change - control of each channel
+config.featselection = 1;
 for k=1:63
     config.prefix = sprintf('%02d_%s_', k, auxchs{k});
     
@@ -35,6 +38,5 @@ for k=1:63
     
     neural_network(config);
 end
-%neural_network_intersubjs(config);
 
 
