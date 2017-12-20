754e02255af0afb01e41e13f0269696e35deef9d
Bruno Melo <bruno.melo@idor.org>
2017-12-20 02:22:28 -0200

    Fix bugs.

--- src/eeg/proc/normalize_eeg.m
+++ src/eeg/proc/normalize_eeg.m
@@ -3,11 +3,11 @@ function signal = normalize_eeg(signal)
 % signal will be normalized by subtracting the mean value and dividing by the variance
 
 meanE = mean(signal);
-varE  = var(signal);
-
 signal = (signal - meanE);
 
-if abs(varE) > 0.1 && abs(varE) < 200
+varE  = var(signal);
+
+if abs(varE) > 0.1% && abs(varE) < 200
     signal = signal / varE;
 else
     disp(varE);
--- tests/connectivity/plot_exported.m
+++ tests/connectivity/plot_exported.m
@@ -1,3 +1,32 @@
+%% Setup of processing
+config = setup('neutral_length', 10);
+config.ignore = {
+     1, [48]
+     2, [18 48]
+     3, [49]
+     4, [9 10 20 31 44 45 54 59]
+     5, [31 34]
+     6, [32 41 63]
+     9, [48]
+    10, [13 59]
+    13, [27]
+    14, [29 56]
+    15, [27]
+};
+
+indir = fullfile( config.outdir_base, 'FEATS' );
+files = utils.resolve_names([indir '/*/l_*']);
+
+for k=1:length(files)
+    file = files{k};
+    newfile = strrep(file, 'FEATS', 'FEATS/CON');
+    basedir = fileparts(newfile);
+    if ~isdir(basedir)
+        mkdir(basedir);
+    end
+    copyfile(file, newfile);
+end
+
 chan_names = {EEG.chanlocs.labels};
 % ---> PDC
 figure('Name','Partial Directed Coherence (PDC)','NumberTitle','off');
